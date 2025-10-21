464d7409339bd8c331f85c1ce0215b762d5d57f7
Rilr20 <nrrl99@gmail.com>
2025-10-21 13:35:24 +0200

    added component to index page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import Tablepage from '$lib/tablepage.svelte';
 </script>
 
-<!-- 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
-<Tablepage />
+<div class="container">
+
+    <Tablepage />
+</div>
